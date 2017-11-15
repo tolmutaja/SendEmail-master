e8ee6132d33adbc3361c0c5d4b25b98085740255
tolumutaja <epicbooman3@gmail.com>
2017-11-15 23:54:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/myapplication/ShowPictureActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ShowPictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_picture);
+    }
+    public void  onBack(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+}
